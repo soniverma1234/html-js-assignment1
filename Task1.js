605b6230b8541de6b710186e65f9edb0186e6aaa
soniverma1234 <soniverma19207@gmail.com>
2026-06-05 14:46:16 +0530

    Add files via upload

---
+++ Task1.js
@@ -0,0 +1,18 @@
+    let name ="Soni Verma";
+     consol.log(name)
+
+     //Number
+     let age =20;
+     consol.log(age)
+
+     //Boolean
+     let Student = true;
+     consol.log(Student)
+
+     //Null
+     let value = null;
+     consol.log(value)
+
+     //Undefined
+     let apple;
+     consol.log(apple)
